e1e69507255dc4d797503c254bc1fa1bf0e35237
Seab <honeyboyseab@gmail.com>
2017-04-08 09:17:04 -0400

    Assigning Constant Properties During Initialization

--- Initialization.playground/Contents.swift
+++ Initialization.playground/Contents.swift
@@ -2,4 +2,21 @@
 
 import UIKit
 
+class SurveyQuestion {
+    
+    var text: String
+    var response: String?
+    
+    init(text: String) {
+        self.text = text
+    }
+    
+    func ask() {
+        print(text)
+    }
+}
 
+    let cheeseQuestion = SurveyQuestion(text: "Do you like cheese?")
+    cheeseQuestion.ask()
+
+// Assigning Constant Properties During Initialization
