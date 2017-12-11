adce730bafabb40c39f1f9693fdcf2afcd59dd79
Seab <honeyboyseab@gmail.com>
2017-12-11 13:14:42 -0500

    add Booleans

--- The Basics.playground/Contents.swift
+++ The Basics.playground/Contents.swift
@@ -107,6 +107,76 @@ let pi2 = Double(three) + pointOneFourOneFiveNine
 let integerPi = Int(pi)
 // interPi equalsl 3, and is inferred to be of type Int
 
+// TODO: Type Aliases
+
+// Booleans
+
+let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious {
+    print("Mmm, tasty turnips!")
+} else {
+    print("Eww, turnips are horrible.")
+}
+
+/*
+ 
+let i = 1
+if i {
+    // this example will not compile, and will report an error.
+}
+ 
+ */
+
+let i = 1
+if i == 1 {
+    // this example will compile successfully
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
