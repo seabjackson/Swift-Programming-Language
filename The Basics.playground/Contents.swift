c4b5287745666b279ff80e4ed5aaa51d676dd9db
Seab <honeyboyseab@gmail.com>
2017-12-11 12:54:43 -0500

    add numeric type conversion

--- The Basics.playground/Contents.swift
+++ The Basics.playground/Contents.swift
@@ -86,6 +86,18 @@ let oneMillion = 1_000_000
 
 // Numeric Type Conversion
 
+// Integer Conversion
+
+// let cannotBeNegative: UInt8 = -1
+// UInt8 cannot store negative numbers, and so this will report an error
+
+// let tooBig: Int8 = Int.max + 1
+// Int8 cannot store a number larger than its maximum value, and so this will also report an error
+
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+
 
 
 
