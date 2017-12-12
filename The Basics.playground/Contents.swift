e2982a181df5018a753b94426657f39a0a6f3abb
Seab <honeyboyseab@gmail.com>
2017-12-11 19:45:36 -0500

    add assertion and preconditions

--- The Basics.playground/Contents.swift
+++ The Basics.playground/Contents.swift
@@ -194,13 +194,22 @@ if let definiteString = assumedString {
     print(definiteString)
 }
 
+// Assertions and Preconditions
 
+// Debugging with Assertions
 
+let age = -3
+//assert(age >= 0, "A person's age can't be less than zero.")
 
+//if age > 10 {
+//    print("You can ride the roller-cast or the ferris wheel.")
+//} else if age > 0 {
+//    print("You can ride the ferris wheel.")
+//} else {
+//    assertionFailure("A person's age can't be less than zero.")
+//}
 
-
-
-
+// Enforcing Preconditions
 
 
 
