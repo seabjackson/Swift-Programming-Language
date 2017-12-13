5179fd2035b1e04377bd037d39571591d2690b67
Seab <honeyboyseab@gmail.com>
2017-12-12 21:49:51 -0500

    add how to compare strings

--- Strings and Characters.playground/Contents.swift
+++ Strings and Characters.playground/Contents.swift
@@ -146,12 +146,53 @@ let beginning = greeting1[..<index1]
 
 let newString = String(beginning)
 
+// Comparing Strings
 
+// String and Character Equality
 
+let quotation1 = "We're a lot alike, you and I."
+let sameQuotation = "We're a lot alike, you and I."
+if quotation1 == sameQuotation {
+    print("These two strings are considered equal.")
+}
 
+// Prefix ans Suffix Equality
 
+let romeoAndJuliet = [
+    "Act 1 Scene 1: Verona, A public place",
+    "Act 1 Scene 2: Capulet's mansion",
+    "Act 1 Scene 3: A room in Capulet's mansion",
+    "Act 1 Scene 4: A street outside Capulet's mansion",
+    "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+    "Act 2 Scene 1: Outside Capulet's mansion",
+    "Act 2 Scene 2: Capulet's orchard",
+    "Act 2 Scene 3: Outside Friar Lawrence's cell",
+    "Act 2 Scene 4: A street in Verona",
+    "Act 2 Scene 5: Capulet's mansion",
+    "Act 2 Scene 6: Friar Lawrence's cell"
+]
 
+var act1SceneCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasPrefix("Act 1 ") {
+        act1SceneCount += 1
+    }
+}
 
+print("There are \(act1SceneCount) scenes in Act 1")
+
+var mansionCount = 0
+var cellCount = 0
+
+for scene in romeoAndJuliet {
+    if scene.hasSuffix("Capulet's mansion") {
+        mansionCount += 1
+    } else if scene.hasSuffix("Friar Lawrence's cell") {
+        cellCount += 1
+    }
+}
+
+print("\(mansionCount) mansion scenes; \(cellCount) cell scenes")
 
 
 
