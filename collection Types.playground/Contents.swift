41247f3e0a4c724f6ed6a9d16510c1c8b2a76c99
Seab <honeyboyseab@gmail.com>
2017-12-14 00:10:54 -0500

    add how to use varying methods on dictionaries

--- collection Types.playground/Contents.swift
+++ collection Types.playground/Contents.swift
@@ -67,3 +67,124 @@ for item in shoppingList {
 for (index, value) in shoppingList.enumerated() {
     print("Item \(index + 1): \(value)")
 }
+
+// Dictionaries
+
+// Creating an Empty Dictionary
+
+var namesOfIntegers = [Int: String]()
+namesOfIntegers[16] = "sixteen"
+namesOfIntegers = [:]
+
+// Creating a Dictionary with a Dictionary Literal
+
+var airports = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+
+// Accessing and Modifying a Dictionary
+
+print("The airports dictionary contains \(airports.count)")
+
+if airports.isEmpty {
+    print("The airports dictionary is empty.")
+} else {
+    print("The airports dictionary is not empty.")
+}
+
+airports["LHR"] = "London"
+airports["LHR"] = "London Heathrow"
+
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
+    print("The old value for DUB was \(oldValue)")
+}
+
+if let airportName = airports["DUB"] {
+    print("The name of the airport is \(airportName)")
+} else {
+    print("That airport is not in the airportss dictionary.")
+}
+
+airports["APL"] = "Apple International"
+airports["APL"] = nil
+
+if let removedValue = airports.removeValue(forKey: "DUB") {
+    print("The removed airport's name is \(removedValue)")
+} else {
+    print("The airport's dictionary does not contain a value for DUB.")
+}
+
+// Iterating Over a Dictionary
+
+for (airportCode, airportName) in airports {
+    print("\(airportCode): \(airportName)")
+}
+
+for airportCode in airports.keys {
+    print("Airport code: \(airportCode)")
+}
+
+for airportName in airports.values {
+    print("Airport name: \(airportName)")
+}
+
+let airportCodes = [String](airports.keys)
+let airportNames = [String](airports.values)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
