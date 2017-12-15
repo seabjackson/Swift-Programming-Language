23062e1bf35e7d4ccdefd82471191ec4ff3c2c61
Seab <honeyboyseab@gmail.com>
2017-12-15 01:23:46 -0500

    add control flow

---
+++ Control Flow.playground/Contents.swift
@@ -0,0 +1,210 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Control Flow
+
+// For-In Loops
+
+let names = ["Anna", "Alex", "Brian", "Jack"]
+
+for name in names {
+    print("Hello, \(name)")
+}
+
+let numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+for (animalName, legCount) in numberOfLegs {
+    print("\(animalName)s have \(legCount) legs")
+}
+
+for index in 1...5 {
+    print("\(index) tines 5 is \(index * 5)")
+}
+
+let base = 3
+let power = 10
+var answer = 1
+
+for _ in 1...power {
+    answer *= base
+}
+
+print("\(base) to the power of \(power) is \(answer)")
+
+// While Loops
+
+let finalSquare = 25
+var board = [Int](repeating: 0, count: finalSquare + 1)
+board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+
+var square = 0
+var diceRoll = 0
+while square < finalSquare {
+    // roll the dice
+    diceRoll += 1
+    if diceRoll == 7 { diceRoll = 1 }
+    // move by the rolled amount
+    square += diceRoll
+    if square < board.count {
+        // if we're still on the board, move up or down for a snake or ladder
+        square += board[square]
+    }
+}
+
+print("Game Over!")
+
+// Repeat-While
+
+// Conditional Statements
+
+// If
+
+var temperatureInFahrenheit = 30
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+}
+
+temperatureInFahrenheit = 40
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else {
+    print("It's not that cold. Wear a t-shirt.")
+}
+
+temperatureInFahrenheit = 90
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else if temperatureInFahrenheit >= 86 {
+    print("It's really warm. Don't forget to wear sunscreen.")
+} else {
+    print("It's not that cold. Wear a t-shirt.")
+}
+
+// Switch
+
+let someCharacter: Character = "z"
+switch someCharacter {
+case "a":
+    print("The first letter of the alphabet")
+case "z":
+    print("The last letter of the alphabet")
+default:
+    print("Some other character")
+}
+
+// No Implicit Fallthrough
+
+let anotherCharacter: Character = "a"
+switch anotherCharacter {
+case "a", "A":
+    print("The letter A")
+default:
+    print("Not the letter A")
+}
+
+// Interval Matching
+
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+let naturalCount: String
+switch approximateCount {
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "several"
+case 12..<100:
+    naturalCount = "dozens of"
+case 100..<1000:
+    naturalCount = "hundreds of"
+default:
+    naturalCount = "many"
+}
+
+print("There are \(naturalCount) \(countedThings).")
+
+// Compound Cases
+
+let someCharacter1: Character = "e"
+switch someCharacter1 {
+case "a", "e", "i", "o", "u":
+    print("\(someCharacter1) is a vowel")
+    case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n",
+         "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
+    print("\(someCharacter1) is a consonant")
+default:
+    print("\(someCharacter1) is not a vowel or consonant")
+}
+
+// Control Transfer Statements
+
+// Break in a Loop Statement
+
+// Break in a Swift Statement
+
+let numberSymbol: Character = "三"
+var possibleIntegerValue: Int?
+
+switch numberSymbol {
+case "1", "١", "一", "๑":
+    possibleIntegerValue = 1
+case "2", "٢", "二", "๒":
+    possibleIntegerValue = 2
+    case "3", "٣", "三", "๓":
+    possibleIntegerValue = 3
+case "4", "٤", "四", "๔":
+    possibleIntegerValue = 4
+default:
+    break
+}
+
+if let integerValue = possibleIntegerValue {
+    print("The integer value of \(numberSymbol) is \(integerValue)")
+}
+
+let integerToDescribe = 5
+var description = "The number \(integerToDescribe) is"
+
+switch integerToDescribe {
+case 2, 3, 5, 7, 11, 13, 17, 19:
+    description += " a prime number, and also "
+    fallthrough
+default:
+    description += " an integer."
+}
+
+print(description)
+
+// Early Exit
+
+func greet(person: [String: String]) {
+    guard let name = person["name"] else {
+        return
+    }
+    
+    print("Hello \(name)")
+    
+    guard let location = person["location"] else {
+        print("I hope the weather is nice near you.")
+        return
+    }
+    
+    print("I hope the weather is nice in \(location).")
+}
+
+greet(person: ["name": "John"])
+
+
+
+
+
+
+
+
+
+
+
+
+
