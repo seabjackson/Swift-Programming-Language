95bb45b522132f1f8eebe2e95a59abaa980995da
Seab <honeyboyseab@gmail.com>
2018-02-17 11:34:20 -0500

    added tuples and typealiases

--- The Basics.playground/Contents.swift
+++ The Basics.playground/Contents.swift
@@ -211,10 +211,31 @@ let age = -3
 
 // Enforcing Preconditions
 
+// Type Aliases
+
+typealias AudioSample = UInt16
+
+var maxAmplitudeFound = AudioSample.min
 
 
+// Tuples
 
+let http404Error = (404, "Not Found")
 
+let (statusCode, statusMessage) = http404Error
+
+print("The status message is \(statusMessage)")
+
+let (justTheStatusCode, _) = http404Error
+print("The status code is \(justTheStatusCode)")
+
+print("The status code is \(http404Error.0)")
+print("The status message is \(http404Error.1)")
+
+let http200Status = (statusCode: 200, description: "OK")
+
+print("The status code is \(http200Status.statusCode)")
+print("The status message is \(http200Status.description)")
 
 
 
