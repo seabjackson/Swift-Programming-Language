ecf615e7b16ec9d3de4e1cd9f93b868726778dae
Seab Jackson <honeyboyseab@gmail.com>
2020-02-21 13:09:54 -0500

    added accessing and mofification of arrays

--- CollectionTypesSwift5.1.playground/Contents.swift
+++ CollectionTypesSwift5.1.playground/Contents.swift
@@ -1,3 +1,54 @@
 import UIKit
 
+// Creating an Empty Array
+
+var someInts = [Int]()
+print("someInts is of type [Int] with \(someInts.count) items.")
+
+someInts.append(3)
+someInts = []
+
+// Creting an Array with Default Value
+var threeDoubles = Array(repeating: 0.0, count: 3)
+
+// Creating an Array by Adding Two Arrays Together
+var anotherThreeDoubles = Array(repeating: 2.5, count: 3)
+var sixDoubles = threeDoubles + anotherThreeDoubles
+
+// Creating an Array with an Array Literal
+var shoppingList = ["Eggs", "Milk"]
+
+// Accessing and Modifying an Array
+print("The shopping list contains \(shoppingList.count) items.")
+
+if shoppingList.isEmpty {
+    print("The shopping list is empty.")
+} else {
+    print("The shopping list is not empty.")
+}
+
+shoppingList.append("Flour")
+
+shoppingList += ["Baking Powder"]
+shoppingList += ["Chocolate Spread", "Cheese", "Butter"]
+
+var firstItem = shoppingList[0]
+
+shoppingList[0] = "Six eggs"
+shoppingList[4...6] = ["Bananas", "Apples"]
+shoppingList.insert("Maple Syrup", at: 0)
+let mapleSyrup = shoppingList.remove(at: 0)
+
+let apples = shoppingList.removeLast()
+for item in shoppingList {
+    print(item)
+}
+
+for (index, value) in shoppingList.enumerated() {
+    print("Item \(index + 1): \(value)")
+}
+
+
+
+
 
